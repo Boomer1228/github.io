013d437116de16de918b1de512345cc93996e7ce
Boomer1228 <collinmchugh09@gmail.com>
2026-05-03 18:10:14 -0400

    Add files via upload

---
+++ FinalAssignment/programs.js
@@ -0,0 +1,5 @@
+function submitEmail() {
+    alert("Thank you for subscribing! We will keep you updated with our latest website additions!")
+}
+
+document.getElementById("button").addEventListener("click", submitEmail)
